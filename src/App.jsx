85538abaec0ec28ef87c32b627fae929a5df2f57
Michael Lally <mikelally801.com>
2025-01-03 20:06:04 -0600

    removed starter code

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
       <h1>Obliger</h1>
       <CassettePlayer />
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <p>
-        Lorem ipsum.
-      </p>
     </>
   )
 }
